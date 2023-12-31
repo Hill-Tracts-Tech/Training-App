3c498e7a9d1069d5a63487f6874eddaf4f355463
Talimul212 <islamtalimul0@gmail.com>
2023-12-31 13:11:18 +0600

    wip

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,45 @@
+/* eslint-disable react/prop-types */
+import Skeleton from "react-loading-skeleton";
+import "./Loader.css";
+const Loader = ({ type }) => {
+  return (
+    <div className="container">
+      {type === "List" ? (
+        <>
+          <Skeleton count={1} height={"40px"} width={"15%"} />
+          <Skeleton count={6} height={"100px"} width={"100%"} />
+        </>
+      ) : type === "bookingDetails" ? (
+        <>
+          <div>
+            <Skeleton count={1} height={"300px"} width={"500px"} />
+            <div>
+              <Skeleton count={1} height={"195px"} width={"600px"} />
+              <Skeleton count={1} height={"100px"} width={"35%"} />
+            </div>
+          </div>
+          <>
+            <Skeleton count={1} height={"220px"} width={"100%"} />
+          </>
+        </>
+      ) : type === "roomDetails" ? (
+        <>
+          <div className="roomDetails">
+            <Skeleton count={1} height={"270px"} width={"400px"} />
+            <Skeleton count={1} height={"270px"} width={"720px"} />
+          </div>
+          <Skeleton count={1} height={"220px"} width={"100%"} />
+        </>
+      ) : type === "restaurantDetails" ? (
+        <div className="restaurantDetails">
+          <Skeleton count={1} height={"430px"} width={"400px"} />
+          <Skeleton count={3} height={"140px"} width={"600px"} />
+        </div>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+};
+
+export default Loader;
